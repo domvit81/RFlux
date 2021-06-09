fb75a873f2a08be77c9c5829833aa224ec176a53
Domenico Vitale <49226595+domvit81@users.noreply.github.com>
2021-06-09 07:34:03 +0200

    Add files via upload

--- R/robf_despiking.R
+++ R/robf_despiking.R
@@ -1,9 +1,10 @@
 
 
 
-robf_despiking <- function(x, mfreq, wsignal = mfreq*30+1, wscale = mfreq*30+1, wby=1, zth=5){
+robf_despiking <- function(x, mfreq, file_length=30){
 	# Window width selection
-	nL <- length(x)
+	nL <- mfreq*60*file_length
+	x <- x[1:nL]
 	mod_rlm <- rlm(as.vector(x)~poly(seq(1,nL,1), 5, raw = FALSE))
 	ind <- which(abs(as.vector(residuals(mod_rlm))) > 3*Qn(as.vector(residuals(mod_rlm))))
 	out_patch1 <- statsNA(replace(x, ind, NA), print_only=FALSE)$longest_na_gap
@@ -17,10 +18,10 @@ robf_despiking <- function(x, mfreq, wsignal = mfreq*30+1, wscale = mfreq*30+1, 
 		}
 	
 	if(wl <= mfreq*60+1){
-		mm <- matrix(x, nrow=mfreq*60*5);
+		mm <- matrix(x, nrow=nL/6);
 		plan(multicore);
 		dspk <- future_apply(mm, MARGIN=2, function(x) despiking(x, mfreq=mfreq, variant="v3", wsignal=wl, wscale=wl, wby=1, zth=5));
-		spike_loc <- na.omit(as.vector(c(dspk[[1]]$spike_loc, dspk[[2]]$spike_loc + mfreq*60*5, dspk[[3]]$spike_loc + mfreq*60*5*2, dspk[[4]]$spike_loc + mfreq*60*5*3, dspk[[5]]$spike_loc + mfreq*60*5*4, dspk[[6]]$spike_lo + mfreq*60*5*5)));
+		spike_loc <- na.omit(as.vector(c(dspk[[1]]$spike_loc, dspk[[2]]$spike_loc + mfreq*60*5, dspk[[3]]$spike_loc + mfreq*60*5*2, dspk[[4]]$spike_loc + mfreq*60*5*3, dspk[[5]]$spike_loc + mfreq*60*5*4, dspk[[6]]$spike_loc + mfreq*60*5*5)));
 		ts_cleaned <- as.vector(c(dspk[[1]]$ts_cleaned, dspk[[2]]$ts_cleaned,dspk[[3]]$ts_cleaned, dspk[[4]]$ts_cleaned, dspk[[5]]$ts_cleaned, dspk[[6]]$ts_cleaned))
 		}
 	
